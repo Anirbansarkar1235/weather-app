0e687fe4b84c2cc4ceb51e10d760bb4774a73510
Anirban Sarkar <anirbansarkar.1235@gmail.com>
2020-08-03 02:55:23 +0530

    Updates

---
+++ precache-manifest.e7f6c8123e082b6ef0799297ac07ab40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905c244c0c918f5dfba76d85c0b6fa22",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a6aba2a13575be86f401",
+    "url": "/weather-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "76d943f49ba8978c22d1",
+    "url": "/weather-app/static/css/main.4e4e7374.chunk.css"
+  },
+  {
+    "revision": "a6aba2a13575be86f401",
+    "url": "/weather-app/static/js/2.e38dc606.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/weather-app/static/js/2.e38dc606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d943f49ba8978c22d1",
+    "url": "/weather-app/static/js/main.aae4564d.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  }
+]);
